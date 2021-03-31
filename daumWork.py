846e2a7921fd57ca28923285206defbc93e18392
leo.woo <leo.woo@kakaocorp.com>
2021-03-31 13:48:05 +0900

    daumWork.py updated as daum portal js selector path updated

--- daumWork.py
+++ daumWork.py
@@ -4,19 +4,19 @@ import datetime
 import json
 from elasticsearch import Elasticsearch, helpers
 
-es = Elasticsearch(hosts="http://news-crawler-es-1426481898.ap-northeast-2.elb.amazonaws.com:9200/")
+#es = Elasticsearch(hosts="http://news-crawler-es-1426481898.ap-northeast-2.elb.amazonaws.com:9200/")
 target_source = "https://m.daum.net/"
 portal = "daum"
 raw = requests.get(target_source, headers={'User-Agent': 'Mozilla/5.0'})
 html = BeautifulSoup(raw.text, "html.parser")
 
-articles = html.select("ul.list_txt.list_rubics.\#news1\#rubics\#txt > li")
+articles = html.select("#channel_news1_top > div:nth-child(1) > div:nth-child(3) > ul > li")
 
 documents = []
 for ar in articles:
     title = ar.select_one("a").text
     provider = ar.select_one("a")['data-tiara-provider']
-    news_id = ar.select_one("a")['data-rubics-news-id']
+    news_id = ar.select_one("a")['data-tesla-news-id']
     documents.append(
         {
             '_index': "daum-news-articles-v1",
